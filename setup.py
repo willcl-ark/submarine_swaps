a9c6639f93453918a5847ff30cee952c9de53377
willcl-ark <will8clark@gmail.com>
2019-07-22 09:22:50 +0100

    Target python 3.6
    Signed-off-by: willcl-ark <will8clark@gmail.com>

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ setuptools.setup(
     url="https://github.com/willcl-ark/submarine_swaps",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     keywords="submarine swaps api",
     install_requires=[],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
--- submarine_api/submarine.py
+++ submarine_api/submarine.py
@@ -5,38 +5,36 @@ import requests
 SUBMARINE_API = "http://localhost:9889/api/v0"
 
 
-def get_quote(network: str, invoice: str, refund:str):
+def get_quote(network: str, invoice: str, refund: str):
     """POST a new swap
     """
-    data = {'network': network,
-            'invoice': invoice,
-            'refund': refund}
+    data = {"network": network, "invoice": invoice, "refund": refund}
     return requests.post(url=f"{SUBMARINE_API}/swaps/", json=data, timeout=30)
 
 
 def check_status(network: str, invoice: str, redeem_script: str):
     """POST a swap check request
     """
-    data = {'network': network,
-            'invoice': invoice,
-            'redeem_script': redeem_script}
+    data = {"network": network, "invoice": invoice, "redeem_script": redeem_script}
     return requests.post(url=f"{SUBMARINE_API}/swaps/check", json=data, timeout=30)
 
 
 def broadcast_tx(network: str, transaction: str):
     """POST a transaction to broadcast to the network
     """
-    data = {'network': network,
-            'transaction': transaction}
+    data = {"network": network, "transaction": transaction}
     return requests.post(url=f"{SUBMARINE_API}/transactions/", json=data, timeout=30)
 
 
 def get_address_details(address: str, network: str):
     """GET details about an address
     """
-    params = {'address': address, 'network': network}
-    return requests.get(url=f"{SUBMARINE_API}/address_details/{network}/{address}",
-                        params=json.dumps(params), timeout=30)
+    params = {"address": address, "network": network}
+    return requests.get(
+        url=f"{SUBMARINE_API}/address_details/{network}/{address}",
+        params=json.dumps(params),
+        timeout=30,
+    )
 
 
 def get_exchange_rates():
@@ -48,9 +46,12 @@ def get_exchange_rates():
 def get_invoice_details(network: str, invoice: str):
     """GET details about an invoice
     """
-    params = {'network': network, 'invoice': invoice}
-    return requests.get(url=f"{SUBMARINE_API}/invoice_details/{network}/{invoice}",
-                        params=json.dumps(params), timeout=30)
+    params = {"network": network, "invoice": invoice}
+    return requests.get(
+        url=f"{SUBMARINE_API}/invoice_details/{network}/{invoice}",
+        params=json.dumps(params),
+        timeout=30,
+    )
 
 
 def get_active_networks():
